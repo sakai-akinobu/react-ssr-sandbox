21c0a2f41526fc008900f7183c1f3ca01e19262e
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 12:49:13 +0900

    Use components.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,43 @@
 import React from 'react';
 
-export default function App() {
-  return <div>App</div>;
+import Counter from '../Counter';
+import IncrementButton from '../IncrementButton';
+import DecrementButton from '../DecrementButton';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.incrementCounter = this.incrementCounter.bind(this);
+    this.decrementCounter = this.decrementCounter.bind(this);
+
+    this.state = {counter: 0};
+  }
+
+  incrementCounter() {
+    this.setState((state) => {
+      return {
+        counter: state.counter + 1,
+      };
+    });
+  }
+
+  decrementCounter() {
+    this.setState((state) => {
+      return {
+        counter: state.counter - 1,
+      };
+    });
+  }
+
+  render() {
+    const {counter} = this.state;
+    return (
+      <>
+        <Counter value={counter} />
+        <IncrementButton onClick={this.incrementCounter} />
+        <DecrementButton onClick={this.decrementCounter} />
+      </>
+    );
+  }
 }
