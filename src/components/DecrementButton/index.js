cd6c5eaba0387957c3f290d46d9282ad8e7cb4c6
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 12:48:23 +0900

    Add components.

---
+++ src/components/DecrementButton/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function DecrementButton(props) {
+  return <button onClick={props.onClick}>-</button>;
+}
---
+++ src/components/IncrementButton/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function IncrementButton(props) {
+  return <button onClick={props.onClick}>+</button>;
+}
