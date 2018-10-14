dbb95c57193f17dc03e80ede9f34fa1dd4c68bef
sakai-akinobu <sakai.akinobu@gmail.com>
2018-10-14 12:27:18 +0900

    Loading script for client.

--- src/components/Html/index.js
+++ src/components/Html/index.js
@@ -3,8 +3,13 @@ import React from 'react';
 export default function Html(props) {
   return (
     <html>
+      <head>
+        <script src="./client.js" defer></script>
+      </head>
       <body>
-        {props.children}
+        <div id="app">
+          {props.children}
+        </div>
       </body>
     </html>
   );
